243146cd42a1412717d2cef1fddbd3a0ac179e15
Vincent Jerry V. Juanico <vincentjerryjuanico@gmail.com>
2026-04-15 13:00:28 +0800

    04142026: V4 : Collisions, Jumping and Gravity | Create an Amazing Game with Flutter & Flame

---
+++ lib/components/player.dart
@@ -0,0 +1,207 @@
+import 'dart:async';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:pixel_adventure/components/collision_block.dart';
+import 'package:pixel_adventure/components/player_hitbox.dart';
+import 'package:pixel_adventure/components/utils.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
+
+enum PlayerState {idle, running, jumping, falling}
+
+class Player extends SpriteAnimationGroupComponent
+    with HasGameRef<PixelAdventure>, KeyboardHandler{
+
+  String character;
+  Player({
+    position,
+    this.character = 'Virtual Guy'
+  }) : super(position: position);
+
+  final double stepTime = 0.05;
+  late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runningAnimation;
+  late final SpriteAnimation jumpingAnimation;
+  late final SpriteAnimation fallingAnimation;
+
+  final double _gravity = 9.8;
+  final double _jumpForce = 250 ;
+  final double _terminalVelocity = 300;
+  double horizontalMovement = 0;
+  double moveSpeed = 100;
+  Vector2 velocity = Vector2.zero();
+  bool isOnGround = false;
+  bool hasJumped = false;
+  List<CollisionBlock> collisionBlocks = [];
+  PlayerHitbox hitBox = PlayerHitbox(
+    offsetX: 10,
+    offsetY: 4,
+    width: 14,
+    height: 28,
+  );
+
+  @override
+  FutureOr<void> onLoad() {
+    _loadAllAnimations();
+    // debugMode = true;
+    add(RectangleHitbox(
+      position: Vector2(hitBox.offsetX, hitBox.offsetY),
+      size: Vector2(hitBox.width, hitBox.height)
+    ));
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    _updatePlayerState();
+    _updatePlayerMovement(dt);
+    _checkHorizontalCollisions();
+    _applyGravity(dt);
+    _checkVerticalCollisions();
+    super.update(dt);
+  }
+  
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    horizontalMovement = 0;
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    horizontalMovement += isLeftKeyPressed ? -1 : 0;
+    horizontalMovement += isRightKeyPressed ? 1 : 0;
+
+    hasJumped = keysPressed.contains(LogicalKeyboardKey.space);
+
+    return super.onKeyEvent(event, keysPressed);
+  }
+
+  void _loadAllAnimations() {
+    idleAnimation = _spriteAnimation('Idle', 11);
+    runningAnimation = _spriteAnimation('Run', 12);
+    jumpingAnimation = _spriteAnimation('Jump', 1);
+    fallingAnimation = _spriteAnimation('Fall', 1);
+
+    // list of all animations
+    animations = {
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation,
+      PlayerState.jumping: jumpingAnimation,
+      PlayerState.falling: fallingAnimation,
+    };
+
+    // Set current animations
+    current = PlayerState.running;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount,) {
+    return SpriteAnimation.fromFrameData(
+        game.images.fromCache('Main Characters/$character/$state (32x32).png'),
+        SpriteAnimationData.sequenced(
+            amount: amount,
+            stepTime: stepTime,
+            textureSize: Vector2.all(32)
+        )
+    );
+  }
+
+  void _updatePlayerState() {
+    PlayerState playerState = PlayerState.idle;
+
+    if(velocity.x < 0 && scale.x > 0) {
+      flipHorizontallyAroundCenter();
+    } else if (velocity.x > 0 && scale.x < 0) {
+      flipHorizontallyAroundCenter();
+    }
+
+    //check if moving , set running
+    if (velocity.x > 0 || velocity.x < 0) playerState = PlayerState.running;
+
+    // Check if Falling set to falling
+    if (velocity.y > _gravity) playerState = PlayerState.falling;
+
+    // Check if Jumping
+    if (velocity.y < 0) playerState = PlayerState.jumping;
+
+    current = playerState;
+  }
+
+  void _updatePlayerMovement(double dt) {
+    if(hasJumped && isOnGround) _playerJump(dt);
+
+    // if (velocity.y > _gravity) isOnGround = false;
+
+    velocity.x =  horizontalMovement * moveSpeed;
+    position.x += velocity.x * dt;
+  }
+
+  void _playerJump(double dt) {
+    velocity.y = -_jumpForce;
+    position.y += velocity.y * dt;
+    isOnGround = false;
+    hasJumped = false;
+  }
+
+  void _checkHorizontalCollisions() {
+    for(final block in collisionBlocks) {
+      // Handle Collisions
+      if(!block.isPlatform){
+        if(checkCollision(this, block)) {
+          if(velocity.x > 0) {
+            velocity.x = 0;
+            position.x = block.x - hitBox.offsetX - hitBox.width;
+            break;
+          }
+          if(velocity.x < 0) {
+            velocity.x = 0;
+            position.x = block.x + block.width + hitBox.width + hitBox.offsetX;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  void _applyGravity(double dt) {
+    velocity.y += _gravity;
+    velocity.y = velocity.y.clamp(-_jumpForce, _terminalVelocity);
+    position.y += velocity.y * dt;
+  }
+
+  void _checkVerticalCollisions() {
+    for(final block in collisionBlocks) {
+      if(block.isPlatform) {
+        if(checkCollision(this, block)) {
+          if (velocity.y > 0) {
+            velocity.y = 0;
+            position.y = block.y - hitBox.height - hitBox.offsetY;
+            isOnGround = true;
+            break;
+          }
+        }
+      } else {
+        if(checkCollision(this, block)) {
+          if(velocity.y > 0) {
+            velocity.y = 0;
+            position.y = block.y - hitBox.height - hitBox.offsetY;
+            isOnGround = true;
+            break;
+          }
+          if(velocity.y < 0){
+            velocity.y = 0;
+            position.y = block.y + block.height - hitBox.offsetY;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+
+
+
+  
+}
